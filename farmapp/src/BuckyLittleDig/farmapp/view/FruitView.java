04164ae3336f9f7e243cf7e427b62589258ac868
Suphawit <suphawit.biggy@gmail.com>
2017-04-14 16:23:33 +0700

    Create Project + some code

---
+++ farmapp/src/BuckyLittleDig/farmapp/view/FruitView.java
@@ -0,0 +1,9 @@
+package BuckyLittleDig.farmapp.view;
+
+/**
+ * @author Suphawit Kasemsirisopha [572115056]
+ * on Apr 5, 2017
+ */
+public class FruitView {
+
+}
---
+++ farmapp/src/BuckyLittleDig/farmapp/view/PlantView.java
@@ -0,0 +1,16 @@
+package BuckyLittleDig.farmapp.view;
+
+/**
+ * @author Suphawit Kasemsirisopha [572115056]
+ *         on Apr 5, 2017
+ */
+public class PlantView {
+    public void viewPlant(int no) {
+
+    }
+
+    public void viewPlants() {
+
+    }
+
+}
